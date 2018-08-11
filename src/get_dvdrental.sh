e0bf91875175de40f5cfd5b79222848090046014
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-08-11 11:24:11 -0700

    unattended install, also install unzip

--- src/get_dvdrental.sh
+++ src/get_dvdrental.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 cd /src
 apt-get update
-apt-get install curl wget
+apt-get install -y curl wget unzip
 curl --output dvdrental.zip http://www.postgresqltutorial.com/wp-content/uploads/2017/10/dvdrental.zip
