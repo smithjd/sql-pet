0844217059d115a797fc5b525a909f7ce187223c
Jim Tyhurst <jim@tyhurst.com>
2018-10-02 08:37:09 -0700

    Minor editing of chapters 2-4.

--- r-database-docker/book-src/db-login-batch-code.R
+++ r-database-docker/book-src/db-login-batch-code.R
@@ -5,13 +5,11 @@
 
 #' Connect to Postgres, waiting if it is not ready
 #'
-#' @export
-#'
 #' @param user Username that will be found
 #' @param password Password that corresponds to the username
 #' @param dbname the name of the database in the database
 #' @param seconds_to_test the number of iterations to try while waiting for Postgres to be ready
-#'
+#' @export
 wait_for_postgres <- function(user, password, dbname, seconds_to_test = 10) {
   for (i in 1:seconds_to_test) {
     db_ready <- DBI::dbCanConnect(RPostgres::Postgres(),
