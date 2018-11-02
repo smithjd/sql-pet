10f2451ad0749cc7cf7c6423b6ade77fcc73bc90
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-11-02 10:53:00 -0700

    make sure newlines are right

--- define_postgresql_params.R
+++ define_postgresql_params.R
@@ -1,13 +1,13 @@
 #! /usr/bin/env Rscript
 
 cat(
-  "DEFAULT_POSTGRES_USER_NAME=postgres\n",
+  "\nDEFAULT_POSTGRES_USER_NAME=postgres",
   file = "~/.Renviron",
   sep = "",
   append = TRUE
 )
 cat(
-  "DEFAULT_POSTGRES_PASSWORD=postgres\n",
+  "\nDEFAULT_POSTGRES_PASSWORD=postgres\n",
   file = "~/.Renviron",
   sep = "",
   append = TRUE
